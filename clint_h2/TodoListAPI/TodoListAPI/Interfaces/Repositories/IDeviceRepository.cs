ca51c75a16df2a64a9bfc55c9fdd8e7852bc273d
AnyaTsy <105385972+AnyaTsy@users.noreply.github.com>
2023-12-03 20:51:26 +0200

    Add files via upload

---
+++ clint_h2/TodoListAPI/TodoListAPI/Interfaces/Repositories/IDeviceRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using TodoListAPIi.Models;
+
+namespace TodoListAPI.Interfaces.Repositories
+{
+    public interface IDeviceRepository
+    {
+        List<Device> GetDevices();
+
+        int GetDevicesTotalCount();
+
+        Device GetDeviceById(int id);
+
+        Device AddDevice(Device device);
+    }
+}
---
+++ clint_h2/TodoListAPI/TodoListAPI/Interfaces/Services/IDeviceService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using TodoListAPI.Models;
+
+namespace TodoListAPI.Interfaces.Services
+{
+    public interface IDeviceService
+    {
+        Device GetDeviceById(int id);
+
+        List<Device> GetDevices(out int totalCount);
+
+        Device AddDevice(Device device);
+    }
+}
